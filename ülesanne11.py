b3c321ed78237fcf95bef5dbd6be1b89eb8425ca
ljatsa <ljatsa@hkhk.edu.ee>
2026-01-13 09:48:51 +0200

    sgdfhbfg

---
+++ ülesanne11.py
@@ -0,0 +1,25 @@
+ # ülesanne 11.1
+#  arv = 5
+#  for i in range(1,6):
+#      print(" " * i + "*" * arv)
+#      arv-=1
+#      
+#     
+#     
+#     
+# ülesanne 11.2
+#  arv = 5
+#  for i in range(1,6):
+#      print("*" * arv)
+#      arv-=1
+# 
+# ülesanne 11.3
+#  arv = 5
+#  for i in range(1,6):
+#      print(" " * arv + "*" * i)
+#      arv-=1
+#     
+# 
+# ülesanne 11.4
+#   for i in range(1,6):
+#       print("*" * i)
--- ülessanne9.py
+++ ülessanne9.py
@@ -1,3 +1,4 @@
+import random
 # #Ülesanne 9.2
 # #15.12.2025
 # #Lisett-Rosette Jätsa
@@ -24,14 +25,98 @@
 #         print("TIK")
 #     elif i%5==0:
 #         print("TAK")
-#         
+#
 
 #9.6
 # Kuva nimekirjast unikaalsed nimed:
-nimed = ['Martin', 'Tõnu', 'Andres', 'Tõnu', 'Andres', 'Andres', 'Andres', 'Tõnu', 'Marko', 'Mari', 'Jüri', 'Liis', 'Marko', 'Piret', 'Anu']
-puhas = []
-for nimi in nimed:
-    if nimi not in puhas:
-        puhas.append(nimi)
-        
-print(puhas)
+# nimed = ['Martin', 'Tõnu', 'Andres', 'Tõnu', 'Andres', 'Andres', 'Andres', 'Tõnu', 'Marko', 'Mari', 'Jüri', 'Liis', 'Marko', 'Piret', 'Anu']
+# puhas = []
+# for nimi in nimed:
+#     if nimi not in puhas:
+#         puhas.append(nimi)
+#         
+# print(puhas)
+# 
+
+# #ülesanne9.8
+# for i in range(11):
+#     print(f"{i} * {i} = {i*i}")
+#     
+    
+    
+    
+#ülesanne9.9 
+# tehe = ["+","-","*","/"]
+# punktid = 0
+# kysimuste_arv = 10
+# for _ in range(kysimuste_arv):
+#     arv1 = random.randint(1,10)
+#     arv2 = random.randint(1,10)
+#     t = random.choice(tehe)
+#     
+#     if t =="+":
+#         print(f"{arv1}{t}{arv2}=")
+#         v = int(input("Vastus: "))
+#         if arv1+arv2 == v:
+#             punktid+=1
+#     elif t =="-":
+#         print(f"{arv1}{t}{arv2}=")
+#         v = int(input("Vastus: "))
+#         if arv1-arv2 == v:
+#             punktid+=1
+#     elif t =="/":
+#         print(f"{arv1}{t}{arv2}=")
+#         v = float(input("Vastus: "))
+#         if round(arv1/arv2,1) == v:
+#             punktid+=1
+#     else:
+#         print(f"{arv1}{t}{arv2}=")
+#         v = int(input("Vastus: "))
+#         if arv1*arv2 == v:
+#             punktid+=1
+# if punktid/kysimuste_arv >=0.5:
+#     print (f"A - {punktid}/{kysimuste_arv}")
+# else:
+#     print (f"MA - {punktid}/{kysimuste_arv}")
+# 
+# print(punktid)
+
+
+
+# ülesanne 11.1
+ arv = 5
+ for i in range(1,6):
+     print(" " * i + "*" * arv)
+     arv-=1
+     
+    
+    
+    
+# ülesanne 11.2
+ arv = 5
+ for i in range(1,6):
+     print("*" * arv)
+     arv-=1
+
+# ülesanne 11.3
+
+ for i in range(1,6):
+     print(" " * arv + "*" * i)
+     arv-=1
+    
+
+# ülesanne 11.4
+  for i in range(1,6):
+      print("*" * i)
+
+
+
+
+
+
+
+
+
+
+
+
