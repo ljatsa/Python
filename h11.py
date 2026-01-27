70dad1c0aca8fe3a10e16a7d3601b16ac6df79b0
ljatsa <ljatsa@hkhk.edu.ee>
2026-01-27 15:43:25 +0200

    m    m

--- h11.py
+++ h11.py
@@ -1,21 +1,49 @@
-# #ülesanne 11.1
+# #ülesanne 11
 # #Lisett-Rosette Jätsa
-# 
-# def pikim_sona(list):
-#     pikimArv = 0
-#     pikimNimi = ""
-#     for i in list:
-#         if len(i) > pikimArv:
-#             pikimArv = len(i)
-#             pikimNimi = i
-#     return pikimNimi
+import turtle
+import random
+def pikim_sona(list):
+    pikimArv = 0
+    pikimNimi = ""
+    for i in list:
+        if len(i) > pikimArv:
+            pikimArv = len(i)
+            pikimNimi = i
+    return pikimNimi
 
-#ülesanne 11.2
+
 def kolm_pikimat_sona(list):
-    if len(nimed) < 3:
-        return sorted(nimed, key = len, reverse = True)
-    return sorted(nimed, key = len, reverse = True)[:3]
+    if len(nimed) > 2:
+        list.sort(key = len, reverse = True)
+    return list [0:3]
 nimed = ["Joosep", "Joonas", "Mario", "Kivakesekesekene","Sergei"]
 
+def viisnurk(k):
+    for i in range(5):
+        turtle.fd(k)
+        turtle.lt(144)
+
+def ring(r):
+    turtle.circle(r)
+
+def ruut(k):
+    for i in range(4):
+        turtle.fd(k)
+        turtle.lt(90)
+
+def suvaline(n):
+    suv = random.randit(1,3)
+    if suv==1:
+        viisnurk(n)
+    elif suv==2:
+        ring(n)
+    else:
+        ruut(n)
+
 print(kolm_pikimat_sona(nimed))
+print(pikim_sona(nimed))
+
+suvaline(100)
+
+turtle.done()
         
