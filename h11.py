9c2bf8f0606a01d2d18df8312de49b9ce4b74237
ljatsa <ljatsa@hkhk.edu.ee>
2026-01-27 14:59:57 +0200

    n gfhn vb vb

--- h11.py
+++ h11.py
@@ -1,22 +1,21 @@
-#ülesanne 11
-#Lisett-Rosette Jätsa
+# #ülesanne 11.1
+# #Lisett-Rosette Jätsa
+# 
+# def pikim_sona(list):
+#     pikimArv = 0
+#     pikimNimi = ""
+#     for i in list:
+#         if len(i) > pikimArv:
+#             pikimArv = len(i)
+#             pikimNimi = i
+#     return pikimNimi
 
-def pikim_sona(list):
-    pikimArv = 0
-    pikimNimi = ""
-    for i in list:
-        if len(i) > pikimArv:
-            pikimArv = len(i)
-            pikimNimi = i
-    return pikimNimi
-
-
+#ülesanne 11.2
 def kolm_pikimat_sona(list):
-    Kui nimekirjas on rohkem kui 2:
-        sorteerin nimekirja pikkuse järgi ja reverse
-        tagastan list [0:3]
+    if len(nimed) < 3:
+        return sorted(nimed, key = len, reverse = True)
+    return sorted(nimed, key = len, reverse = True)[:3]
+nimed = ["Joosep", "Joonas", "Mario", "Kivakesekesekene","Sergei"]
 
-nimed = ["Joosep", "Joonas", "Mario", "Kivakesekesekene"]
-print(pikim_sona(nimed))
 print(kolm_pikimat_sona(nimed))
         
